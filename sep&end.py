efc8f15fd7617e2eb5e0da82a285120a726bed28
Ashutosh Gupta <ashutoshgupta8840@gmail.com>
2022-12-02 18:01:21 +0000

    Added comments in sep&send.py

--- sep&end.py
+++ sep&end.py
@@ -1,2 +1,5 @@
+# sep is used to specity a character to be printed in between each value given to print function.
+# end is used to specity what should be printed on the console after each print.
+
 print("hello World", 1, 12.12, 'A', sep="~", end="--Ashu--\n");
 # print("Welcome my first python program in 100 day of python bootcamp.");
