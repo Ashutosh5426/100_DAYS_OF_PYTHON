b451a15af82d175097975b27efd3a91837c06474
Ashutosh Gupta <ashutoshgupta8840@gmail.com>
2022-12-03 13:47:13 +0000

    Added a file datatypes.py :

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+# DATATYPES IN PYTHON ->
+
+#   1. Numeric Data- int, float, complex
+
+a = 1; #int
+b = 3.14 #float
+c = complex(2, 5) #complex
+
+  # 2. Text Data- str
+
+c = "Ashutosh Gupta" #string
+
+  # 3. Boolean Data- 
+
+d = True #boolean
+
+  # 4. Sequenced Data- list, tuple
+
+e = [1, 2.3, "456", 'A', "~"] #list
+f = (2, 3, 45, "4343", 434, 2, 3) #tuple
+# Note- tuple in not changeable.
+
+  # 5. Mapped Data- dictionary
+g = {"name": "Santosh", "age": 32, 1: "KNB"} #dictionary
+
+# +++++++++++++++++++++EVERYTHING THAT WE SEE IN PYTHON IS AN OBJECT+++++++++++++++++++++
